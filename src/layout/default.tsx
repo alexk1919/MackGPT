e5966eb94801d9d8b6c79c9e3844c7cded3db8fa
Alexander Kim <54913409+alexk1919@users.noreply.github.com>
2023-04-20 13:03:49 -0700

    frontend tweaks

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -15,43 +15,39 @@ const DefaultLayout = (props: LayoutProps) => {
   return (
     <div
       className={clsx(
-        "flex flex-col bg-gradient-to-b from-[#2B2B2B] to-[#1F1F1F]",
+        "flex flex-col bg-gradient-to-b from-[#4A3A2E] to-[#3A2A1E]",
         props.centered && "items-center justify-center"
       )}
     >
       <Head>
-        <title>AgentGPT</title>
+        <title>MAckGPT</title>
         <meta name="description" content={description} />
-        <meta name="twitter:site" content="@AgentGPT" />
+        <meta name="twitter:site" content="@MackGPT_com" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="AgentGPT 🤖" />
+        <meta name="twitter:title" content="MackGPT 🤖" />
         <meta name="twitter:description" content={description} />
         <meta
           name="twitter:image"
-          content="https://agentgpt.reworkd.ai/banner.png"
+          content="https://mackgpt.com/banner.png"
         />
         <meta name="twitter:image:width" content="1280" />
         <meta name="twitter:image:height" content="640" />
         <meta
           property="og:title"
-          content="AgentGPT: Autonomous AI in your browser 🤖"
+          content="MackGPT: Autonomous AI in your browser 🤖"
         />
         <meta
           property="og:description"
           content="Assemble, configure, and deploy autonomous AI Agents in your browser."
         />
-        <meta property="og:url" content="https://agentgpt.reworkd.ai/" />
+        <meta property="og:url" content="https://mackgpt.com/" />
         <meta
           property="og:image"
-          content="https://agentgpt.reworkd.ai/banner.png"
+          content="https://mackgpt.com/banner.png"
         />
         <meta property="og:image:width" content="1280" />
         <meta property="og:image:height" content="640" />
         <meta property="og:type" content="website" />
-        <meta
-          name="google-site-verification"
-          content="sG4QDkC8g2oxKSopgJdIe2hQ_SaJDaEaBjwCXZNkNWA"
-        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <DottedGridBackground
