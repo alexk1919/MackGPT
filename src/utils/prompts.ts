2c58e5b5761244b6d206e5d1f3c048a8aaaed1dd
Alex Kim <54913409+alexk1919@users.noreply.github.com>
2023-04-27 12:27:13 -0700

    prompt tweaks

--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -28,7 +28,7 @@ export const createModel = (settings: ModelSettings) => {
 
 export const startGoalPrompt = new PromptTemplate({
   template:
-    "You are an autonomous task creation AI called MackGPT. You have the following objective `{goal}`. Create a list of zero to five tasks to be completed by your AI system such that your goal is more closely reached or completely reached. Return the response as an array of strings that can be used in JSON.parse()",
+    "You are an autonomous task creation AI called MackGPT. You have the following objective `{goal}`. Create a list of tasks to be completed by your AI system such that your goal is more closely reached or completely reached. Return the response as an array of strings that can be used in JSON.parse()",
   inputVariables: ["goal"],
 });
 
@@ -40,6 +40,6 @@ export const executeTaskPrompt = new PromptTemplate({
 
 export const createTasksPrompt = new PromptTemplate({
   template:
-    "You are an AI task creation agent. You have the following objective `{goal}`. You have the following incomplete tasks `{tasks}` and have just executed the following task `{lastTask}` and received the following result `{result}`. Based on this, create a new task to be completed by your AI system ONLY IF NEEDED such that your goal is more closely reached or completely reached. Return the response as an array of strings that can be used in JSON.parse() and NOTHING ELSE",
+    "You are an AI task creation agent called MackGPT. You have the following objective `{goal}`. You have the following incomplete tasks `{tasks}` and have just executed the following task `{lastTask}` and received the following result `{result}`. Based on this, create a new task to be completed by your AI system ONLY IF NEEDED such that your goal is more closely reached or completely reached. Return the response as an array of strings that can be used in JSON.parse() and NOTHING ELSE",
   inputVariables: ["goal", "tasks", "lastTask", "result"],
 });
